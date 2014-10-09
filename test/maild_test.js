e47c3d78b5e367227e9df55cfbbb50198859cca5
Ben Sack <ben@sack.io>
2014-10-09 17:32:05 -0400

    added test suite

--- test/maild_test.js
+++ test/maild_test.js
@@ -2,39 +2,47 @@
 
 var Belt = require('jsbelt')
   , Optionall = require('optionall')
-  , O = new Optionall()
+  , Path = require('path')
+  , O = new Optionall({'__dirname': Path.resolve(module.filename + '/../..')})
   , Async = require('async')
   , _ = require('underscore')
-  , Mailedit = new require('../lib/maild.js')(O)
-  , Marketing = Mailedit.marketing
+  , Maild = new require('../lib/maild.js')(O)
 ;
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
-
-exports['mailchimp'] = {
+exports['email_io'] = {
   setUp: function(done) {
     return done();
   },
-  'mailchimp': function(test) {
-    test.ok(true);
-    return test.done();
+  'tests': function(test) {
+    var gb = {};
+
+    return Async.waterfall([
+      function(cb){
+        gb.email = {
+          'from': O.from_email
+        , 'to': O.to_email
+        , 'subject': Belt.uuid()
+        , 'html': Belt.uuid()
+        , 'text': Belt.uuid()
+        };
+
+        Maild.incoming.on('email', function(email){
+          test.ok(email.subject === gb.email.subject);
+          test.ok(email.html === gb.email.html);
+          test.ok(email.text === gb.email.text);
+          test.ok(Belt._get(email, 'to.0.address') === gb.email.to);
+          test.ok(Belt._get(email, 'from.0.address') === gb.email.from);
+          return cb();
+        });
+        return Maild.outgoing.send_email(gb.email, function(err){
+          test.ok(!err);
+          return;
+        });
+      }
+    ], function(err){
+      if (err) console.error(err);
+      test.ok(!err);
+      return test.done();
+    });
   },
 };
